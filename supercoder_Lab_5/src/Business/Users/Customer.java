e7a2e0650bf6e2e01d468b4d92937d0282762e32
Joy <zhang.dawe@husky.neu.edu>
2018-10-18 19:38:03 -0400

    supercoder_Lab_5 v1.0 by Joy

---
+++ supercoder_Lab_5/src/Business/Users/Customer.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Users;
+
+import Business.Abstract.User;
+import java.util.Date;
+
+/**
+ *
+ * @author harshalneelkamal
+ */
+public class Customer {
+    private String name;
+    private String role;
+    private String customerusername;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getCustomerusername() {
+        return customerusername;
+    }
+
+    public void setCustomerusername(String customerusername) {
+        this.customerusername = customerusername;
+    }
+    
+}
