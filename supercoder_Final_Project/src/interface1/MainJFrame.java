4b13a618f655c39dc4283ff71059fc54382bfae5
Billyire <jiang.hong@husky.neu.edu>
2018-12-08 03:02:13 -0500

    Final_Project v5.0 by Hongbo Jiang

--- supercoder_Final_Project/src/interface1/MainJFrame.java
+++ supercoder_Final_Project/src/interface1/MainJFrame.java
@@ -221,7 +221,14 @@ public class MainJFrame extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(MainJFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
-
+        try
+        {
+            org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
+        }
+        catch(Exception e)
+        {
+            //TODO exception
+        }
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
